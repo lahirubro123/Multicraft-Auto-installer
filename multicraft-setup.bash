559944dc024fa6365e40259a0494b16302a28c52
Josh Q <josh@quinlan.cloud>
2021-04-06 02:15:44 +0100

    Final flourishes

--- multicraft-setup.bash
+++ multicraft-setup.bash
@@ -23,7 +23,7 @@ echo -e "[SUCCESS] Packages installed!"
 
 echo -e "[INFO] Starting MySQL setup..."
 mysql_secure_installation
-echo -e "\n [SUCCESS] MySQL setup complete!"
+echo -e "\n[SUCCESS] MySQL setup complete!"
 
 echo -e "[INFO] Installing more required packages..."
 apt-get -qq install php libapache2-mod-php php-mysql php-pdo php-sqlite3 php-curl php-xml php-gd default-jre default-jdk -y &> /dev/null
@@ -46,9 +46,10 @@ echo -e "[SUCCESS] Multicraft downloaded. Prepare to complete configuration!\n"
 tar xvzf multicraft.tar.gz &> /dev/null
 cd multicraft
 ./setup.sh
-read -p "\n Enter your database password (the last one you specified): " dbpass
+echo " "
+read -p "Enter your database password (the last one you specified): " dbpass
 
-echo -e "\n [INFO] Setting up database..."
+echo -e "\n[INFO] Setting up database..."
 mysql -e "create database multicraft_panel" &> /dev/null
 mysql -e "create database multicraft_daemon" &> /dev/null
 mysql -e "create user 'multicraft'@'localhost' identified by '${dbpass}'" &> /dev/null
